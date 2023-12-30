aa38741bb8efee9e33f62f1a5846a264d6c54435
yfujit <yfujita813@gmail.com>
2023-12-31 06:55:59 +0900

    change responsive design

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center width-full">
+    <footer className="bg-gray-800 text-white p-4 text-center width-full text-xs sm:text-xs md:text-base lg:text-lg xl:text-xl">
       <p>&copy; 2024 Toshiyuki. All rights reserved.</p>
     </footer>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,20 @@ const BASE_PATH = basePath ? basePath : ""
 
 const Home: React.FC = () => {
   return (
-    <div className="items-center justify-center bg-gray-100">
+    <div className="items-center justify-center bg-black">
       <div className="text-center relative">
         <img
           src={`${BASE_PATH}/happy_new_year_image.png`}
           alt="Happy New Year"
           className="mb-4"
         />
-        <div className="absolute bottom-0 right-0 p-4 text-white">
-          <h1 className="text-6xl font-bold mb-4">Happy New Year 2024 &nbsp;&nbsp;&nbsp;&nbsp;</h1>
-        <p className="text-lg text-white">
+        <div className="absolute bottom-0 right-0 p-4 text-white text-center">
+            <h1 className="sm:text-4xl md:text-5xl lg:text-7xl xl:text-7xl font-bold">
+              Happy New Year 2024 !!
+            </h1>
+        <p className="text-xs sm:text-xs md:text-base lg:text-lg xl:text-xl ">
           謹んで新年のご挨拶を申し上げます<br />
-          昨年はいろいろとお世話になり誠にありがとうございました<br />
+          昨年は大変お世話になり誠にありがとうございました<br />
           本年も何卒よろしくお願い申し上げます
         </p>
         </div>
